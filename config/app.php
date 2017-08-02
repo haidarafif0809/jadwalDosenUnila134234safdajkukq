147250793fa4f842b860cd34ab4cfd5d4fcb0373
Unknown <ghozam.alghifari96@gmail.com>
2017-08-02 09:31:44 +0700

    Menambahkan Package untuk datatable

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Laratrust\LaratrustServiceProvider::class,
         Yajra\Datatables\DatatablesServiceProvider::class, 
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -228,6 +229,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Laratrust' => Laratrust\LaratrustFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
