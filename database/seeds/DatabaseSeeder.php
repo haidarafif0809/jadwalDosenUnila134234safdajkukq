b03bd4c28da41724989604a123faf06739def3b4
RindangRamadhan <rindangramadhan10@gmail.com>
2017-08-02 13:23:58 +0700

    Master Data Block
    Membuar sample data (Seeder) master data block

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(MasterRuanganSeeder::class);
         $this->call(MasterMataKuliahSeeder::class);
+        $this->call(MasterBlockSeeder::class);
     }
 }
