37e47e52d4d3641a88440cd45f6322d81683eb29
Unknown <ghozam.alghifari96@gmail.com>
2017-08-23 11:57:03 +0700

    Memperbaik Form Edit User

--- resources/views/master_users/_form.blade.php
+++ resources/views/master_users/_form.blade.php
@@ -9,7 +9,7 @@
 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
 	{!! Form::label('email', 'Username', ['class'=>'col-md-2 control-label']) !!}
 	<div class="col-md-4">
-		{!! Form::email('email', null, ['class'=>'form-control','required','autocomplete'=>'off']) !!}
+		{!! Form::text('email', null, ['class'=>'form-control','required','autocomplete'=>'off']) !!}
 		{!! $errors->first('email', '<p class="help-block">:message</p>') !!}
 	</div>
 </div>
