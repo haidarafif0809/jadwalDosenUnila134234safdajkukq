48a38034a743a44e860c10084e5369ef489070a1
Unknown <ghozam.alghifari96@gmail.com>
2017-09-06 19:51:11 +0700

    Membuat Halaman Dasbor Penjadwalan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,39 @@
 @extends('layouts.app')
-
 @section('content')
 
-@if (Auth::guest())
-@else
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+<style type="text/css">
+    #filter {
+        margin-top: 10px;
+    }
+</style>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <ul class="breadcrumb">
+                <li><a href="{{ url('/home') }}">Home</a></li>
+                <li class="active">Penjadwalan</li>
+            </ul>
+ 
+            
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Penjadwalan</h2>
+                </div>
 
-                    <div class="panel-body">
-                        You are logged in!
+                <div class="panel-body"> 
+                     <a class="btn btn-primary" href="{{ url('/') }}">Hari Ini</a>
+                     <a class="btn btn-primary" href="{{ url('/besok') }}">Besok</a>
+                     <a class="btn btn-primary" href="{{ url('/lusa') }}">Lusa</a>
+                    <div class="table-responsive"> 
+                    {!! $html->table(['class'=>'table-striped table']) !!}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-@endif
+</div>
 @endsection
-    </body>
-</html>
+
+@section('scripts')
+{!! $html->scripts() !!} 
+@endsection
