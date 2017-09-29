2ba2ddb42ebb7035f33b5d6f1b752af96c4650ed
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-09-29 17:09:31 +0700

    Tambahan Kolom Jenis Kelompok di  (Kelompok Mahasiswa)
    Tambahan Kolom Jenis Kelompok di  (Kelompok Mahasiswa)

--- app/KelompokMahasiswa.php
+++ app/KelompokMahasiswa.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class KelompokMahasiswa extends Model
 {
     //
-        protected $fillable = ['nama_kelompok_mahasiswa','id_angkatan'];
+        protected $fillable = ['nama_kelompok_mahasiswa','id_angkatan','jenis_kelompok'];
 
         	public function angkatan()
 		  {
