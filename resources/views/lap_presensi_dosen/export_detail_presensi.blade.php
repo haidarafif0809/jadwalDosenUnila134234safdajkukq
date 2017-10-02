787e62342a7f40cd27c73262454adf9cd606a245
Riko Pernando <rikopernando96@gmail.com>
2017-10-02 17:07:10 +0700

    Perbaikan Hasil Sprint  Demo dan menambahkan Materi di Laporan Detail Presensi Dosen
    Perbaikan Hasil Sprint  Demo dan menambahkan Materi di Laporan Detail Presensi Dosen

--- resources/views/lap_presensi_dosen/export_detail_presensi.blade.php
+++ resources/views/lap_presensi_dosen/export_detail_presensi.blade.php
@@ -19,7 +19,13 @@
         <tr>
            <td>{{$detail_presensi->nama_dosen}}</td>
            <td>{{$detail_presensi->tipe_jadwal}}</td>
+
+           @if($detail_presensi->tipe_jadwal == 'CSL' || $detail_presensi->tipe_jadwal == 'TUTORIAL')
+           <td>{{ App\Materi::select('nama_materi')->where('id',$detail_presensi->id_materi)->first()->nama_materi }} </td>
+           @else 
            <td>{{$detail_presensi->nama_mata_kuliah}}</td>
+           @endif 
+
            <td>{{$detail_presensi->ruangan}}</td>
            <td>{{$detail_presensi->waktu}}</td>
            <td>{{$detail_presensi->jarak_absen}} m</td>
