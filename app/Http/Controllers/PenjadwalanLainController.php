828aab6b3a291019aed162027947451391a3406a
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-10-06 14:42:39 +0700

    Perbaikan penjadwalan proses pertemuan boleh salah satu saja

--- app/Http/Controllers/PenjadwalanLainController.php
+++ app/Http/Controllers/PenjadwalanLainController.php
@@ -75,33 +75,153 @@ class PenjadwalanLainController extends Controller
     public function store(Request $request){
 
         $this->validate($request, [
-            'tanggal'   => 'required',
-            'data_waktu'     => 'required', 
-            'tanggal_2'   => 'required',
-            'data_waktu_2'     => 'required', 
             'id_block'    => 'required|exists:master_blocks,id',
             'id_ruangan'    => 'required|exists:master_ruangans,id',
             'id_user'    => 'required|exists:users,id',
             'id_materi'    => 'required',
             'id_kelompok' => 'required'
-        ]);   
+        ]);
+        if($request->data_waktu == "" AND $request->tanggal == "" AND $request->data_waktu_2 == "" AND $request->tanggal_2 == ""){
+            Session::flash("flash_notification", [
+                        "level"=>"danger",
+                        "message"=>"Tanggal Pertemuan dan Waktu Pertemuan Harus di Isi"
+                        ]);
+             
+            return redirect()->back()->withInput();
+        }
 
         //jenis kelompok 
         $jenis_kelompok = $request->jenis_kelompok;
         //jenis kelompok 
 
+        if($request->data_waktu == "" AND $request->tanggal == "" AND $request->data_waktu_2 != "" AND $request->tanggal_2 != ""){
+            //MEMISAHKAN WAKTU MULAI DAN SELESAIA
+            $data_setting_waktu_2 = explode("-",$request->data_waktu_2);
 
-        //MEMISAHKAN WAKTU MULAI DAN SELESAIA
-        $data_setting_waktu = explode("-",$request->data_waktu);
-        $data_setting_waktu_2 = explode("-",$request->data_waktu_2);
-  
-        //MENGECEK PENJADWLAN
-        $data_penjadwalan = Penjadwalan::statusRuangan($request,$data_setting_waktu);  
+            //MENGECEK PENJADWLAN
+            $data_penjadwalan_2 = Penjadwalan::statusRuangan($request,$data_setting_waktu_2); 
 
-        $data_penjadwalan_2 = Penjadwalan::statusRuangan($request,$data_setting_waktu_2); 
+
+                    //APABILA $data_penjadwalan == 0 maka ngecek dosen
+        if ($data_penjadwalan_2->count() == 0) { 
+
+            //MENGECEK DOSEN DI JADWALAN YANG SAMA
+            $dosen_punya_jadwal = array();
+                foreach ($request->id_user as $user_dosen) {
+
+                 $jadwal_dosen_2 = Jadwal_dosen::statusDosen($request,$user_dosen,$data_setting_waktu_2); 
+                 $data_jadwal_dosen_2 = $jadwal_dosen_2->first(); 
+
+                if ($jadwal_dosen_2->count() > 0) {
+                      array_push($dosen_punya_jadwal,
+                     ['id_jadwal'=>$data_jadwal_dosen_2->id_jadwal,
+                     'id_dosen'=>$data_jadwal_dosen_2->id_dosen]);
+                }
+            } 
+            //APABILA JADWAL NYA SAMA MAKA MUNCUL PERINGATAN
+                if (count($dosen_punya_jadwal) > 0 ) { 
+                    $message = 'Tidak Bisa Menambahkan Dosen Berikut Karena Sudah Memiliki Jadwal :<ul>'; 
+
+                     foreach ($dosen_punya_jadwal as $dosen_punya_jadwals) {  
+                            $nama_dosen = User::find($dosen_punya_jadwals['id_dosen']);
+                            $data_penjadwalans = Penjadwalan::find($dosen_punya_jadwals['id_jadwal']); 
+
+                            $message .= "<li><b>$nama_dosen->name</b> Memilik Jadwal Di Ruangan <b>".$data_penjadwalans->ruangan->nama_ruangan." </b> Block <b>".$data_penjadwalans->block->nama_block."</b></li>";
+                            
+                        }
+                    $message .= '</ul>';
+
+                    Session::flash("flash_notification", [
+                        "level"=>"danger",
+                        "message"=>"$message"
+                        ]); 
+                    return redirect()->back()->withInput();
+                }
+        }
+        else{
+            //APABILA RUANGAN SUDAH DI PAKAI DI WAKTU YANG BERSAMAAN MAKA MUNCUL ALERT DI BAWAH
+            $data_ruangan =  Master_ruangan::find($request->id_ruangan);
+            $data_block = Master_block::find($request->id_block);
+
+                    Session::flash("flash_notification", [
+                        "level"=>"danger",
+                        "message"=>"Ruangan $data_ruangan->nama_ruangan Sudah Di Pakai Block $data_block->nama_block"
+                        ]);
+             
+            return redirect()->back()->withInput();
+        } 
+
+        }
+        elseif($request->data_waktu_2 == "" AND $request->tanggal_2 == "" AND $request->data_waktu != "" AND $request->tanggal != ""){
+            //MEMISAHKAN WAKTU MULAI DAN SELESAIA
+            $data_setting_waktu = explode("-",$request->data_waktu);
+
+            //MENGECEK PENJADWLAN
+             $data_penjadwalan = Penjadwalan::statusRuangan($request,$data_setting_waktu);  
 
         //APABILA $data_penjadwalan == 0 maka ngecek dosen
+        if ($data_penjadwalan->count() == 0) { 
+
+            //MENGECEK DOSEN DI JADWALAN YANG SAMA
+            $dosen_punya_jadwal = array();
+                foreach ($request->id_user as $user_dosen) {
+
+                 $jadwal_dosen = Jadwal_dosen::statusDosen($request,$user_dosen,$data_setting_waktu); 
+                 $data_jadwal_dosen = $jadwal_dosen->first(); 
+
+                if ($jadwal_dosen->count() > 0) {
+                      array_push($dosen_punya_jadwal,
+                     ['id_jadwal'=>$data_jadwal_dosen->id_jadwal,
+                     'id_dosen'=>$data_jadwal_dosen->id_dosen]);
+                }
+            } 
+            //APABILA JADWAL NYA SAMA MAKA MUNCUL PERINGATAN
+                if (count($dosen_punya_jadwal) > 0 ) { 
+                    $message = 'Tidak Bisa Menambahkan Dosen Berikut Karena Sudah Memiliki Jadwal :<ul>'; 
+
+                     foreach ($dosen_punya_jadwal as $dosen_punya_jadwals) {  
+                            $nama_dosen = User::find($dosen_punya_jadwals['id_dosen']);
+                            $data_penjadwalans = Penjadwalan::find($dosen_punya_jadwals['id_jadwal']); 
+
+                            $message .= "<li><b>$nama_dosen->name</b> Memilik Jadwal Di Ruangan <b>".$data_penjadwalans->ruangan->nama_ruangan." </b> Block <b>".$data_penjadwalans->block->nama_block."</b></li>";
+                            
+                        }
+                    $message .= '</ul>';
+
+                    Session::flash("flash_notification", [
+                        "level"=>"danger",
+                        "message"=>"$message"
+                        ]); 
+                    return redirect()->back()->withInput();
+                }
+        }
+        else{
+            //APABILA RUANGAN SUDAH DI PAKAI DI WAKTU YANG BERSAMAAN MAKA MUNCUL ALERT DI BAWAH
+            $data_ruangan =  Master_ruangan::find($request->id_ruangan);
+            $data_block = Master_block::find($request->id_block);
+
+                    Session::flash("flash_notification", [
+                        "level"=>"danger",
+                        "message"=>"Ruangan $data_ruangan->nama_ruangan Sudah Di Pakai Block $data_block->nama_block"
+                        ]);
+             
+            return redirect()->back()->withInput();
+        } 
+            
+        }
+        else{
+            //MEMISAHKAN WAKTU MULAI DAN SELESAIA
+            $data_setting_waktu = explode("-",$request->data_waktu);
+            $data_setting_waktu_2 = explode("-",$request->data_waktu_2);
+
+            //MENGECEK PENJADWLAN
+            $data_penjadwalan = Penjadwalan::statusRuangan($request,$data_setting_waktu);  
+
+            $data_penjadwalan_2 = Penjadwalan::statusRuangan($request,$data_setting_waktu_2); 
+
+                    //APABILA $data_penjadwalan == 0 maka ngecek dosen
         if ($data_penjadwalan->count() == 0 OR $data_penjadwalan_2->count() == 0) { 
+
             //MENGECEK DOSEN DI JADWALAN YANG SAMA
             $dosen_punya_jadwal = array();
                 foreach ($request->id_user as $user_dosen) {
@@ -158,23 +278,15 @@ class PenjadwalanLainController extends Controller
              
             return redirect()->back()->withInput();
         } 
+               
+        }
 
      
-        //JIKA PENJADWALAN TIDAK ADA YANG SAMA MAKA PROSES TAMBAH PENJADWALAN BERHASIL
+        //JIKA WAKTU DAN TANGGAL  PERTEMUAN SATU KOSONG  YANG MASUKAN PERTEMUAN DUA
 
-        $penjadwalan = Penjadwalan::create([ 
-            'tanggal' =>$request->tanggal,
-            'waktu_mulai'=>$data_setting_waktu[0],
-            'waktu_selesai'=>$data_setting_waktu[1],
-            'id_block'=>$request->id_block,
-            'id_materi'=>$request->id_materi,
-            'tipe_jadwal'=>$jenis_kelompok,
-            'id_mata_kuliah'=>"-",
-            'id_ruangan'=>$request->id_ruangan,
-        	'id_kelompok'=>$request->id_kelompok]);
+        if($request->data_waktu == "" AND $request->tanggal == ""){
 
-
-        $penjadwalan_2 = Penjadwalan::create([ 
+            $penjadwalan_2 = Penjadwalan::create([ 
             'tanggal' =>$request->tanggal_2,
             'waktu_mulai'=>$data_setting_waktu_2[0],
             'waktu_selesai'=>$data_setting_waktu_2[1],
@@ -183,10 +295,71 @@ class PenjadwalanLainController extends Controller
             'tipe_jadwal'=>$jenis_kelompok,
             'id_mata_kuliah'=>"-",
             'id_ruangan'=>$request->id_ruangan,
-        	'id_kelompok'=>$request->id_kelompok]);
+            'id_kelompok'=>$request->id_kelompok]);
+        }
+       //JIKA WAKTU DAN TANGGAL  PERTEMUAN DUA KOSONG  YANG MASUKAN PERTEMUAN SATU
+        elseif($request->data_waktu_2 == "" AND $request->tanggal_2 == ""){
+
+            $penjadwalan = Penjadwalan::create([ 
+            'tanggal' =>$request->tanggal,
+            'waktu_mulai'=>$data_setting_waktu[0],
+            'waktu_selesai'=>$data_setting_waktu[1],
+            'id_block'=>$request->id_block,
+            'id_materi'=>$request->id_materi,
+            'tipe_jadwal'=>$jenis_kelompok,
+            'id_mata_kuliah'=>"-",
+            'id_ruangan'=>$request->id_ruangan,
+            'id_kelompok'=>$request->id_kelompok]);
+
+        }
+        //JIKA WAKTU DAN TANGGAL  PERTEMUAN DUA & PERTEMUAN SATU DIISI MASUKAN SEMUA
+        else{
+
+            $penjadwalan_2 = Penjadwalan::create([ 
+            'tanggal' =>$request->tanggal_2,
+            'waktu_mulai'=>$data_setting_waktu_2[0],
+            'waktu_selesai'=>$data_setting_waktu_2[1],
+            'id_block'=>$request->id_block,
+            'id_materi'=>$request->id_materi,
+            'tipe_jadwal'=>$jenis_kelompok,
+            'id_mata_kuliah'=>"-",
+            'id_ruangan'=>$request->id_ruangan,
+            'id_kelompok'=>$request->id_kelompok]);
+
+            $penjadwalan = Penjadwalan::create([ 
+            'tanggal' =>$request->tanggal,
+            'waktu_mulai'=>$data_setting_waktu[0],
+            'waktu_selesai'=>$data_setting_waktu[1],
+            'id_block'=>$request->id_block,
+            'id_materi'=>$request->id_materi,
+            'tipe_jadwal'=>$jenis_kelompok,
+            'id_mata_kuliah'=>"-",
+            'id_ruangan'=>$request->id_ruangan,
+            'id_kelompok'=>$request->id_kelompok]);
+
+        }
 
         //UNTUK MEMBUAT JADWAL DOSEN YANG BERKAIT SAMA PENJADWALAN
-            foreach ($request->id_user as $user_dosen) { 
+   foreach ($request->id_user as $user_dosen) {
+
+             //JIKA WAKTU DAN TANGGAL  PERTEMUAN SATU KOSONG  YANG MASUKAN PERTEMUAN DUA
+
+        if($request->data_waktu == "" AND $request->tanggal == ""){
+ 
+                     $jadwal_dosen_2 = Jadwal_dosen::create([ 
+                    'id_jadwal' =>$penjadwalan_2->id,
+                    'id_dosen'=>$user_dosen,
+                    'id_block'=>$request->id_block,
+                    'id_mata_kuliah'=>"-",
+                    'id_ruangan'=>$request->id_ruangan,
+                    'tanggal' =>$request->tanggal_2,
+                    'waktu_mulai'=>$data_setting_waktu_2[0],
+                    'waktu_selesai'=>$data_setting_waktu_2[1],
+                    ]);
+            }
+       //JIKA WAKTU DAN TANGGAL  PERTEMUAN DUA KOSONG  YANG MASUKAN PERTEMUAN SATU
+        elseif($request->data_waktu_2 == "" AND $request->tanggal_2 == ""){
+
                 $jadwal_dosen = Jadwal_dosen::create([ 
                     'id_jadwal' =>$penjadwalan->id,
                     'id_dosen'=>$user_dosen,
@@ -196,9 +369,23 @@ class PenjadwalanLainController extends Controller
                     'tanggal' =>$request->tanggal,
                     'waktu_mulai'=>$data_setting_waktu[0],
                     'waktu_selesai'=>$data_setting_waktu[1],
-                    ]); 
+                    ]);
+        }
+        //JIKA WAKTU DAN TANGGAL  PERTEMUAN DUA & PERTEMUAN SATU DIISI MASUKAN SEMUA
+        else{
 
-                 $jadwal_dosen_2 = Jadwal_dosen::create([ 
+            $jadwal_dosen = Jadwal_dosen::create([ 
+                    'id_jadwal' =>$penjadwalan->id,
+                    'id_dosen'=>$user_dosen,
+                    'id_block'=>$request->id_block,
+                    'id_mata_kuliah'=>"-",
+                    'id_ruangan'=>$request->id_ruangan,
+                    'tanggal' =>$request->tanggal,
+                    'waktu_mulai'=>$data_setting_waktu[0],
+                    'waktu_selesai'=>$data_setting_waktu[1],
+                    ]);
+
+            $jadwal_dosen_2 = Jadwal_dosen::create([ 
                     'id_jadwal' =>$penjadwalan_2->id,
                     'id_dosen'=>$user_dosen,
                     'id_block'=>$request->id_block,
@@ -207,8 +394,10 @@ class PenjadwalanLainController extends Controller
                     'tanggal' =>$request->tanggal_2,
                     'waktu_mulai'=>$data_setting_waktu_2[0],
                     'waktu_selesai'=>$data_setting_waktu_2[1],
-                    ]);                    
-            }
+                    ]); 
+        
+        }                   
+    }
         //ALERT JIKA BERHASIL
         Session::flash("flash_notification", [
             "level"=>"success",
