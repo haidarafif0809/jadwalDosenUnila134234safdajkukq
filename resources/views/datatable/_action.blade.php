6a2f7ec43f45988e116ab339590b30f4eafe06d1
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-10-18 08:54:27 +0700

    ketika di edit buka halaman baru

--- resources/views/datatable/_action.blade.php
+++ resources/views/datatable/_action.blade.php
@@ -1,7 +1,7 @@
 @role('admin') 
 {!! Form::model($model, ['url' => $form_url, 'method' => 'delete', 'class' => 'form-inline js-confirm', 'data-confirm' => $confirm_message]) !!}
 
-<a href="{{ $edit_url }}" class="btn btn-sm btn-success">Ubah</a> |
+<a href="{{ $edit_url }}"  target="_blank" class="btn btn-sm btn-success">Ubah</a> |
 
 {!! Form::submit('Hapus',['class'=>'btn btn-sm btn-danger js-confirm']) !!}
 {!! Form::close() !!} 
